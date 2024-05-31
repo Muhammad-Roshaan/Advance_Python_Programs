2cbe86dda14d6fd8675cf94f712051d2c299c1d5
Muhammad Roshan Riaz <167735124+Muhammad-Roshaan@users.noreply.github.com>
2024-05-31 23:42:30 +0500

    Add files via upload

---
+++ special operators(is,in).py
@@ -0,0 +1,20 @@
+#membership in 
+
+x = ["Ali", "Hamza"]
+print("Ali" in x)
+
+# not in 
+x = ["Ali", "Hamza"]
+print("Haroon" not in x)
+
+
+
+#Identity is
+
+x = ["Ali", "Taimoor"]
+y = ["Ali", "Taimoor"]
+z = x
+print(x is z)
+print(x is y)
+print(x == y)
+
